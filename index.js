ed76dba33b241efbb848aecf876fa07bdcf7a9d7
Maheshbabu <maheshbabun076@gmail.com>
2024-12-15 22:16:49 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+// Optional: Add interactivity with JavaScript
+document.getElementById('cta-button').addEventListener('click', function () {
+    alert('Welcome! We are excited to have you. Proceeding to registration...');
+});
